064c7054472316c9fa90c2c38d6be4c0fc300d93
Jake Overall <joverall22@gmail.com>
2020-01-17 02:43:05 -0700

    auth0 wrapper

---
+++ index.js
@@ -0,0 +1,276 @@
+import createAuth0Client from "@auth0/auth0-spa-js";
+
+/** Define a default action to perform after authentication */
+const DEFAULT_REDIRECT_CALLBACK = () =>
+  window.history.replaceState({}, document.title, window.location.pathname);
+
+let instance;
+
+/** Returns the current instance of the SDK */
+export const getInstance = () => instance;
+
+/** Creates an instance of the Auth0 SDK. If one has already been created, it returns that instance */
+export const useAuth0 = (
+  {
+    onRedirectCallback = DEFAULT_REDIRECT_CALLBACK,
+    redirectUri = window.location.origin,
+    ...options
+  },
+  Vue
+) => {
+  if (instance) return instance;
+
+  // The 'instance' is simply a Vue object
+  instance = new Vue({
+    data() {
+      return {
+        loading: true,
+        isAuthenticated: false,
+        user: {},
+        userInfo: {},
+        identity: {},
+        bearer: "",
+        auth0Client: null,
+        popupOpen: false,
+        error: null
+      };
+    },
+    methods: {
+      /** Authenticates the user using a popup window */
+      async loginWithPopup(o) {
+        this.popupOpen = true;
+
+        try {
+          await this.auth0Client.loginWithPopup(o);
+        } catch (e) {
+          // eslint-disable-next-line
+          console.error(e);
+        } finally {
+          this.popupOpen = false;
+        }
+
+        this.user = await this.auth0Client.getUser();
+        this.getUserData();
+        this.isAuthenticated = true;
+      },
+      /** Handles the callback when logging in using a redirect */
+      async handleRedirectCallback() {
+        this.loading = true;
+        try {
+          await this.auth0Client.handleRedirectCallback();
+          this.user = await this.auth0Client.getUser();
+          this.isAuthenticated = true;
+          this.getUserData();
+        } catch (e) {
+          this.error = e;
+        } finally {
+          this.loading = false;
+        }
+      },
+      /** Authenticates the user using the redirect method */
+      loginWithRedirect(o) {
+        return this.auth0Client.loginWithRedirect(o);
+      },
+      /** Returns all the claims present in the ID token */
+      getIdTokenClaims(o) {
+        return this.auth0Client.getIdTokenClaims(o);
+      },
+      /** Returns the access token. If the token is invalid or missing, a new one is retrieved */
+      getTokenSilently(o) {
+        return this.auth0Client.getTokenSilently(o);
+      },
+      hasPermissions(permissions) {
+        if (!Array.isArray(permissions)) {
+          permissions = [permissions];
+        }
+        if (!this.identity.permissions) {
+          return false;
+        }
+        while (permissions.length) {
+          let next = permissions.pop();
+          let found = this.identity.permissions.find(p => p == next);
+          if (!found) {
+            return false;
+          }
+        }
+        return true;
+      },
+      hasRoles(roles) {
+        if (!Array.isArray(roles)) {
+          roles = [roles];
+        }
+        if (!this.userInfo.roles) {
+          return false;
+        }
+        while (roles.length) {
+          let next = roles.pop();
+          let found = this.userInfo.roles.find(r => r == next);
+          if (!found) {
+            return false;
+          }
+        }
+        return true;
+      },
+
+      async getIdentityClaims(token) {
+        this.identity = JSON.parse(decodeToken(token));
+        return this.identity;
+      },
+
+      /** Gets the access token using a popup window */
+      getTokenWithPopup(o) {
+        return this.auth0Client.getTokenWithPopup(o);
+      },
+      async getUserData() {
+        try {
+          this.auth0Client;
+          let token = await this.getTokenSilently();
+          let identity = await this.getIdentityClaims(token);
+          this.bearer = "Bearer " + token;
+          let res = await fetch(`https://${options.domain}/userinfo`, {
+            headers: {
+              authorization: this.bearer
+            }
+          });
+
+          let userData = await res.json();
+          for (var key in userData) {
+            let keep = key;
+            if (key.includes("https")) {
+              keep = keep.slice(keep.lastIndexOf("/") + 1);
+            }
+            this.$set(this.userInfo, keep, userData[key]);
+          }
+        } catch (e) {
+          console.error(e);
+        }
+      },
+      /** Logs the user out and removes their session on the authorization server */
+      logout(o) {
+        let logout = this.auth0Client.logout(o);
+        this.bearer = "";
+        this.user = {};
+        this.userInfo = {};
+        this.identity = {};
+        this.isAuthenticated = false;
+        return logout;
+      }
+    },
+    /** Use this lifecycle method to instantiate the SDK client */
+    async created() {
+      // Create a new instance of the SDK client using members of the given options object
+      this.auth0Client = await createAuth0Client({
+        domain: options.domain,
+        client_id: options.clientId,
+        audience: options.audience,
+        redirect_uri: redirectUri
+      });
+
+      try {
+        // If the user is returning to the app after authentication..
+        if (
+          window.location.search.includes("code=") &&
+          window.location.search.includes("state=")
+        ) {
+          // handle the redirect and retrieve tokens
+          const { appState } = await this.auth0Client.handleRedirectCallback();
+
+          // Notify subscribers that the redirect callback has happened, passing the appState
+          // (useful for retrieving any pre-authentication state)
+          onRedirectCallback(appState);
+        }
+      } catch (e) {
+        this.error = e;
+      } finally {
+        // Initialize our internal authentication state
+        this.isAuthenticated = await this.auth0Client.isAuthenticated();
+        this.user = await this.auth0Client.getUser();
+        this.loading = false;
+      }
+    }
+  });
+
+  return instance;
+};
+
+export const authGuard = (to, from, next) => {
+  const fn = () => {
+    // If the user is authenticated, continue with the route
+    if (instance.isAuthenticated) {
+      return next();
+    }
+
+    // Otherwise, log in
+    instance.loginWithRedirect({ appState: { targetUrl: to.fullPath } });
+  };
+
+  // If loading has already finished, check our auth state using `fn()`
+  if (!instance.loading) {
+    return fn();
+  }
+
+  // Watch for the loading property to change before we check isAuthenticated
+  instance.$watch("loading", loading => {
+    if (loading === false) {
+      return fn();
+    }
+  });
+};
+export const onAuth = () => {
+  return new Promise((resolve, reject) => {
+    const authService = getInstance();
+    if (authService.isAuthenticated) {
+      return resolve();
+    }
+    authService.$watch("loading", loading => {
+      if (authService.isAuthenticated === false) {
+        return reject();
+      }
+      return resolve();
+    });
+  });
+};
+
+function b64DecodeUnicode(str) {
+  return decodeURIComponent(
+    atob(str).replace(/(.)/g, function(m, p) {
+      var code = p
+        .charCodeAt(0)
+        .toString(16)
+        .toUpperCase();
+      if (code.length < 2) {
+        code = "0" + code;
+      }
+      return "%" + code;
+    })
+  );
+}
+function decodeToken(str = "") {
+  str = str.split(".")[1];
+  var output = str.replace(/-/g, "+").replace(/_/g, "/");
+  switch (output.length % 4) {
+    case 0:
+      break;
+    case 2:
+      output += "==";
+      break;
+    case 3:
+      output += "=";
+      break;
+    default:
+      throw "Illegal base64url string!";
+  }
+
+  try {
+    return b64DecodeUnicode(output);
+  } catch (err) {
+    return atob(output);
+  }
+}
+
+// Create a simple Vue plugin to expose the wrapper object throughout the application
+export const Auth0Plugin = {
+  install(Vue, options) {
+    Vue.prototype.$auth = useAuth0(options, Vue);
+  }
+};
---
+++ readme.md
@@ -0,0 +1,80 @@
+# @bcwdev/auth0-vue
+
+This library is a small wrapper around auth0-spa-js. It is extended beyond the simple scale that is used in the auth0 tutorial. Added features include getting the `userInfo` and `identity` on login.
+
+> REQUIRED: Enable RBAC in you application for the full set of features in this app. You can also add the following rule to retrieve more information in your userInfo
+
+```javascript
+// AUTH0 RULE
+function (user, context, callback) {
+  // please note auth0 will strip any non namespaced properties
+  const namespace = 'https://YOURDOMAIN.auth0.com';
+  const assignedRoles = (context.authorization || {}).roles;
+
+  let idTokenClaims = context.idToken || {};
+
+  idTokenClaims[`${namespace}/roles`] = assignedRoles;
+  context.idToken = idTokenClaims;
+  context.idToken[namespace + '/user_metadata'] = user.user_metadata;
+  context.idToken[namespace + '/app_metadata'] = user.app_metadata;
+
+  // namespaced properties are reduced to simple properties by this libary
+  // so in vue you can access userInfo.app_metadata directly
+
+  callback(null, user, context);
+}
+```
+
+Fetching this extra data allows us to extend the basic methods for validating user roles and permissions.
+
+On login `$auth.getUserData()` will be triggered
+
+### \$auth
+
+You can access any of the following state properties directly from your vue components using `this.$auth`
+
+```javascript
+// $auth state
+{
+  loading: true,
+  isAuthenticated: false,
+  user: {},
+  userInfo: {},
+  identity: {},
+  bearer: "",
+}
+
+// $auth methods
+{
+  /**
+   * Sets userInfo, identity and bearer
+   * @returns {Promise<void>}
+  */
+  getUserData()
+
+  /**
+   * Depends on UserData
+   * @param {string[] | string} permissions
+   * @returns {boolean}
+  */
+ hasPermissions(permissions) {},
+
+  /**
+   * Depends on UserData
+   * @param {string[] | string} roles
+   * @returns {boolean}
+  */
+  hasRoles(roles) {},
+}
+
+```
+
+> Caution: `$auth.identity` is easily modifed on the client and therefore cannot be trusted when making server side decisions. Your server should use the bearer token and the auth0 api when handeling requests [see @bcwdev/auth0provider]('https://www.npmjs.com/package/@bcwdev/auth0provider')
+
+Conditional rendering in vue templates based on roles or permissions can be accomplished directly from the `$auth` property
+
+```html
+<div v-if="$auth.hasRoles('admin')">
+  <p>Only an admin can see me</p>
+</div>
+```
