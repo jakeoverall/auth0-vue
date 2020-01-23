1122da606f3a496e71bef6dc952c21bae8c97454
Jake Overall <joverall22@gmail.com>
2020-01-23 15:55:56 -0700

    get userInfo on login

--- index.js
+++ index.js
@@ -185,6 +185,7 @@ export const useAuth0 = (
         // Initialize our internal authentication state
         this.isAuthenticated = await this.auth0Client.isAuthenticated();
         this.user = await this.auth0Client.getUser();
+        await this.auth0Client.getUserData();
         this.loading = false;
       }
     }
